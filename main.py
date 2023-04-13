6eba2529344e8b29ae4363b7c0ed803e2be04e29
Sequeira12 <brunomsequeira@hotmail.com>
2023-04-13 16:28:03 +0100

    Exercicio 2.1 FEITO

--- main.py
+++ main.py
@@ -7,27 +7,43 @@ Tomás Dias: 2020215701
 import numpy as np
 import librosa as lb
 
+
+# exercicio 1
+
 def read_csv():
 
+    data = np.genfromtxt(
+        'Features - Audio MER/top100_features.csv', dtype=np.str_, delimiter=",")
+
+    linhas, colunas = np.shape(data)
+
+    fNames = data[0, 1:colunas - 1]
+
+    data = data[1::, 1:(colunas - 1)].astype(float)
+
+    dataNormalizada = normaliza(data)
+
+    np.savetxt("Features - Audio MER/top100_features_normalized.csv",
+               dataNormalizada, fmt="%lf", delimiter=",")
 
 
+def normaliza(data):
+    fn = np.zeros(data.shape)
+    for i in range(len(data[0])):
+        max = np.max(data[::, i])
+        min = np.min(data[::, i])
+        if (max == min):
+            fn[::, i] = 0
+        else:
+            fn[::, i] = (data[::, i] - min)/(max - min)
 
-
-def normaliza():
-    #calcular minimo
-    #calcular maximo
-    if(min == max) fn=0
-
-    np.savetxt()
-
-
-
+    return fn
 
 
 def main():
-    pass
+    read_csv()
 
 
 if __name__ == "__main__":
+
     main()
-    
